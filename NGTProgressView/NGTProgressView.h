31bdae4939338a283df2efd521a3c29fe570d92c
Naveen George Thoppan <naveenjbl@gmail.com>
2017-01-03 13:26:35 +0530

    Initial Commit

---
+++ NGTProgressView/NGTProgressView.h
@@ -0,0 +1,19 @@
+//
+//  NGTProgressView.h
+//  NGTProgressView
+//
+//  Created by Naveen George Thoppan on 03/01/17.
+//  Copyright © 2017 Naveen George Thoppan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NGTProgressView.
+FOUNDATION_EXPORT double NGTProgressViewVersionNumber;
+
+//! Project version string for NGTProgressView.
+FOUNDATION_EXPORT const unsigned char NGTProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NGTProgressView/PublicHeader.h>
+
+
